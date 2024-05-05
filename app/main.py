c57e7cd4de785919d6f1d64273629514a4150079
Wesley <rademakerwesley@gmail.com>
2024-05-05 17:03:49 +0200

    finished development

--- app/main.py
+++ app/main.py
@@ -1,34 +1,33 @@
-from datetime import date
-
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
-from transformers import pipeline
+from uniplot import histogram
+
+from backend.pipeline import main
 
 app = FastAPI()
 
 
-# Define the request model for the endpoint
 class EvaluateRequest(BaseModel):
     model: str = Field(
         ...,
         example="whisper-small",
         description="The model to evaluate, either 'whisper-small' or 'whisper-tiny'.",
     )
-    created_from_date: date = Field(
-        ...,
+    created_from_date: str = Field(
+        None,
         example="2023-01-01",
         description="The minimum date of the recordings to evaluate.",
     )
-    created_to_date: date = Field(
-        ...,
+    created_to_date: str = Field(
+        None,
         example="2023-01-10",
         description="The maximum date of the recordings to evaluate.",
     )
     user_id: int = Field(
-        None, example=123, description="Filter recordings to evaluate by user id."
+        None, example=2, description="Filter recordings to evaluate by user id."
     )
     unit_id: int = Field(
-        None, example=456, description="Filter recordings to evaluate by unit id."
+        None, example=3, description="Filter recordings to evaluate by unit id."
     )
 
 
@@ -39,25 +38,17 @@ class EvaluationResponse(BaseModel):
     )
 
 
-def load_model(model_name: str):
-    if model_name not in ["whisper-small", "whisper-tiny"]:
-        raise ValueError("Model not supported")
-    return pipeline("automatic-speech-recognition", model=model_name)
-
-
-@app.post("/evaluate", response_model=EvaluationResponse)
+@app.post("/evaluate")
 async def evaluate(request: EvaluateRequest):
     try:
-        # Load the specified model
-        asr_model = load_model(request.model)
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        result = main(
+            model_id=request.model,
+            created_from_date=request.created_from_date,
+            created_to_date=request.created_to_date,
+            user_id=request.user_id,
+            unit_id=request.unit_id,
+        )
 
-    # Here we would fetch and filter the dataset based on `created_from_date`, `created_to_date`, `user_id`, `unit_id`.
-    # For now, we're just simulating an evaluation.
-
-    # Simulate fetching data and performing the evaluation
-    # This part should interact with actual data and compute WER
-    simulated_wer = 0.05  # Example WER, this should be computed based on actual data
-
-    return EvaluationResponse(wer=simulated_wer)
+        return result
+    except Exception as e:
+        return {"error": str(e)}
---
+++ backend/pipeline.py
@@ -0,0 +1,187 @@
+import argparse
+from typing import Dict, List
+
+import jiwer
+import numpy as np
+import pandas as pd
+import torch
+from datasets import Audio, load_dataset
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+
+METADATA_PATH = "data/recordings.csv"
+MAX_TOKENS: int = 128
+
+
+def filter_data(
+    df: pd.DataFrame,
+    created_from_date: str = None,
+    created_to_date: str = None,
+    user_id: int = None,
+    unit_id: int = None,
+) -> List[str]:
+    """Filter the dataset based on the provided criteria and return the indecises of the selected recordings"""
+    df["created_at"] = pd.to_datetime(df["created_at"])
+
+    conditions = (
+        (
+            (df["created_at"] >= created_from_date)
+            if created_from_date is not None
+            else True
+        ),
+        (
+            (df["created_at"] <= created_to_date)
+            if created_to_date is not None
+            else True
+        ),
+        (df["user_id"] == user_id) if user_id is not None else True,
+        (df["unit_id"] == unit_id) if unit_id is not None else True,
+    )
+
+    # Apply filters
+    filtered_df = df[
+        (conditions[0]) & (conditions[1]) & (conditions[2]) & (conditions[3])
+    ]
+
+    return filtered_df["recording_id"].to_list()
+
+
+def get_dataset(rows: List, sampling_rate: int = 1600) -> Dict:
+    common_voice = load_dataset(
+        "mozilla-foundation/common_voice_11_0",
+        "nl",
+        split="test",
+        trust_remote_code=True,
+    )
+    common_voice = common_voice.remove_columns(
+        [
+            "accent",
+            "age",
+            "client_id",
+            "down_votes",
+            "gender",
+            "locale",
+            "path",
+            "segment",
+            "up_votes",
+        ]
+    )
+
+    common_voice = common_voice.cast_column("audio", Audio(sampling_rate=sampling_rate))
+
+    return common_voice[rows]
+
+
+def evaluate(
+    dataset: Dict[List, str], record_ids: List, pipeline, scoring_metric=jiwer.wer
+):
+    """transcribe and evaluate a dataset"""
+    results = []
+    for audio, reference, idx in zip(dataset["audio"], dataset["sentence"], record_ids):
+        result = pipeline(audio, generate_kwargs={"language": "dutch"})["text"]
+        score = scoring_metric(result, reference)
+        results.append(
+            {
+                "recording_id": idx,
+                "transcript": result,
+                "original": reference,
+                "score": score,
+                "metric": scoring_metric.__name__,
+            }
+        )
+    return results
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument(
+        "--model_id",
+        type=str,
+        default=None,
+        choices=["whisper-small", "whisper-tiny"],
+        help="The ID of the model",
+    )
+    parser.add_argument(
+        "--created_from_date",
+        type=str,
+        help="The start date of the recordings to evaluate. See Data section for more information.",
+    )
+    parser.add_argument(
+        "--created_to_date",
+        type=str,
+        default=None,
+        help="The end date of the recordings to evaluate. See Data section for more information.",
+    )
+    parser.add_argument(
+        "--user_id",
+        type=int,
+        default=None,
+        help="Filter recordings to evaluate by user id. See Data section for more information.",
+    )
+    parser.add_argument(
+        "--unit_id",
+        type=int,
+        default=None,
+        help="Filter recordings to evaluate by unit id. See Data section for more information.",
+    )
+
+    return parser.parse_args()
+
+
+def main(
+    model_id: str = None,
+    created_from_date: str = None,
+    created_to_date: str = None,
+    user_id: int = None,
+    unit_id: int = None,
+):
+    model_id = f"openai/{model_id}"
+    print(model_id, unit_id, user_id, created_from_date, created_to_date)
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id,
+        torch_dtype=torch_dtype,
+        low_cpu_mem_usage=True,
+        use_safetensors=True,
+    )
+    model.to(device)
+
+    processor = AutoProcessor.from_pretrained(model_id)
+
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        max_new_tokens=MAX_TOKENS,
+        torch_dtype=torch_dtype,
+        device=device,
+    )
+
+    pdf = pd.read_csv(METADATA_PATH)
+    indx_records = filter_data(
+        pdf,
+        created_from_date=created_from_date,
+        created_to_date=created_to_date,
+        user_id=user_id,
+        unit_id=unit_id,
+    )
+    if not indx_records:
+        print("No records identified that meet fitlering criteria")
+        return None
+
+    dataset = get_dataset(rows=indx_records)
+    results = evaluate(dataset, record_ids=indx_records, pipeline=pipe)
+    return results
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(
+        model_id=args.model_id,
+        created_from_date=args.created_from_date,
+        created_to_date=args.created_to_date,
+        user_id=args.user_id,
+        unit_id=args.unit_id,
+    )
